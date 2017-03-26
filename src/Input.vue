f7720c0a78c286dc681ca3655fc5779cc3b4d013
Pradeep Sethi <psethi@gmail.com>
2017-03-26 15:20:36 +0530

    updated the app for message input component

---
+++ src/Input.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<input type="text" name="" v-model="message">
+		<p>{{ message }}</p>
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default {
+		data() {
+			return {
+				message: ''
+			}
+		}
+	}
+</script>
---
+++ src/Message.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<h1>This is a great Message!</h1>
+		<app-input></app-input>
+	</div>
+</template>
+
+<script type="text/javascript">
+	import Input from './Input.vue'
+	export default {
+		components: {
+			'app-input': Input
+		}
+	}  
+</script>
